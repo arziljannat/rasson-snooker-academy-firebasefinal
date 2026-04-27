f965080291ec08ad1c41662158283f25029cc1fa
arziljannat <arziljannat2015@gmail.com>
2026-04-27 17:06:12 +0500

    Update tables.js

--- frontend/scripts/tables.js
+++ frontend/scripts/tables.js
@@ -1572,21 +1572,21 @@ summaryBody.innerHTML = `
     </tr>
 
     
-${combined ? `
-<tr class="combined-row">
-    <td>Combined</td>
-    <td>${combined.gameTotal}</td>
-    <td>${combined.canteenTotal}</td>
-    <td>${combined.gameCollection}</td>
-    <td>${combined.canteenCollection}</td>
-    <td>${combined.gameBalance}</td>
-    <td>${combined.canteenBalance}</td>
-    <td>${combined.expenses}</td>
-    <td>${combined.closingCash}</td>
-    <td>-</td>
-    <td>-</td>
-</tr>
-` : ""}
+${combined ? (
+"<tr class='combined-row'>" +
+"<td>Combined</td>" +
+"<td>" + combined.gameTotal + "</td>" +
+"<td>" + combined.canteenTotal + "</td>" +
+"<td>" + combined.gameCollection + "</td>" +
+"<td>" + combined.canteenCollection + "</td>" +
+"<td>" + combined.gameBalance + "</td>" +
+"<td>" + combined.canteenBalance + "</td>" +
+"<td>" + combined.expenses + "</td>" +
+"<td>" + combined.closingCash + "</td>" +
+"<td>-</td>" +
+"<td>-</td>" +
+"</tr>"
+) : ""}
 
 
     showPopup("shiftSummaryPopup");
