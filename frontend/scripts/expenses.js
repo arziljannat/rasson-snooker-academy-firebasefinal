c0271a2d8828f8dfbfac9b10dda848c846ef2c11
arziljannat <arziljannat2015@gmail.com>
2026-04-28 22:32:33 +0500

    Update expenses.js

--- frontend/scripts/expenses.js
+++ frontend/scripts/expenses.js
@@ -59,6 +59,9 @@ function formatTime(timestamp) {
     }
 
     return date.toLocaleString("en-PK", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
         hour: "2-digit",
         minute: "2-digit",
         hour12: true,
